842efedbb0b377fdd2673d5d4932b5085fff4d72
CouperBR <douglasmsp@hotmail.com>
2020-07-31 19:17:03 -0300

    Layout features.

--- BAIMI/Controllers/ProdutoController.cs
+++ BAIMI/Controllers/ProdutoController.cs
@@ -256,6 +256,7 @@ namespace BAIMI.Controllers
         {
             try
             {
+                ViewBag.HideNavBar = true;
                 var produto = db.produto.Find(id);
                 return View(produto);
             }
